38776e6746121e3e8352c6a105f802c73066a689
Florent Guichard <guichardflorent@gmail.com>
2012-08-12 02:58:36 +0200

    patch for cpu idle behaviour

--- drivers/cpuidle/governors/menu.c
+++ drivers/cpuidle/governors/menu.c
@@ -237,6 +237,8 @@ static int menu_select(struct cpuidle_device *dev)
 	unsigned int power_usage = -1;
 	int i;
 	int multiplier;
+	
+	struct timespec t;
 
 	if (data->needs_update) {
 		menu_update(dev);
@@ -252,8 +254,7 @@ static int menu_select(struct cpuidle_device *dev)
 
 	/* determine the expected residency time, round up */
 	data->expected_us =
-	    DIV_ROUND_UP((u32)ktime_to_ns(tick_nohz_get_sleep_length()), 1000);
-
+	    t.tv_sec * USEC_PER_SEC + t.tv_nsec / NSEC_PER_USEC;
 
 	data->bucket = which_bucket(data->expected_us);
 
