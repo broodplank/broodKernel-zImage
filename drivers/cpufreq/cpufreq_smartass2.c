d1387f78978cae0e381e36a2cdca084a898b37ea
Florent Guichard <guichardflorent@gmail.com>
2012-08-12 03:52:29 +0200

    smartassV2 adjustment

--- drivers/cpufreq/cpufreq_smartass2.c
+++ drivers/cpufreq/cpufreq_smartass2.c
@@ -74,7 +74,7 @@ static unsigned int ramp_down_step;
 /*
 * CPU freq will be increased if measured load > max_cpu_load;
 */
-#define DEFAULT_MAX_CPU_LOAD 50
+#define DEFAULT_MAX_CPU_LOAD 80   * Less agressive 
 static unsigned long max_cpu_load;
 
 /*
