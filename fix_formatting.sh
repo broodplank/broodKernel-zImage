deee3edd142de614bfe9b392bdb765272dc8de1d
Florent Guichard <guichardflorent@gmail.com>
2012-09-08 03:20:37 +0300

    Update to fix_formatting.sh
    Added Auto Chown and Chmod 755, change "broodplank" to your username!

--- fix_formatting.sh
+++ fix_formatting.sh
@@ -1,3 +1,11 @@
-#!/system/bin/sh
+#!/bin/sh
+
+sudo apt-get install dos2unix
 
 find . -type f -exec dos2unix {} +
+
+sudo chown -R broodplank .
+
+sudo chmod -R 755 .
+
+echo "Done with converting all files to unix format, changing ownership of files and setting 755 chmod"
